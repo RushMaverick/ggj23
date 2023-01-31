dab2610db24e7aaecc4fe487fc0cd450a0680d17
Tuomas <tuomas.jaasalo@alceon.fi>
2023-01-31 18:38:47 +0200

    Added basic player movement and follow camera

---
+++ scripts/Player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+@export var movement_speed = 6
+
+func _ready():
+	print("Hello")
+	$AnimationPlayer.play("Idle")
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("back"):
+		direction.z += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		look_at(position + direction, Vector3.UP)
+	velocity.x = direction.x * movement_speed
+	velocity.z = direction.z * movement_speed
+	move_and_slide()
