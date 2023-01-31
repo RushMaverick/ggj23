d7df03cc7285f88f6b6bfdd332a9d2172091755f
Rasmus Rask <rrask@Rasmuss-MacBook-Pro.local>
2023-01-31 21:06:03 +0200

    Cylinder Area3D added, signal check ok

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -2,9 +2,16 @@ extends CharacterBody3D
 
 @export var move_speed = 20
 
+@onready var cyl = $ShapeCast3D
 
 func _physics_process(delta):
 	velocity = Vector3.FORWARD * move_speed * delta
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 	move_and_slide()
-	look_at_from_position(transform.origin, )
+
+func _process(_delta):
+	for	body in get_tree().get_nodes_in_group("target_to_attack"):
+		var _body_pos = body.global_transform.origin
+
+func _on_area_3d_body_entered(body):
+	print(body)
