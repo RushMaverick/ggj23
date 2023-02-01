082e57cdc95c0444e486582b008050ac9c245030
Tuomas <tuomas.jaasalo@alceon.fi>
2023-02-01 18:20:08 +0200

    Some cleanup

--- scripts/FollowCamera.gd
+++ scripts/FollowCamera.gd
@@ -3,4 +3,5 @@ extends Node3D
 @export var target: Node3D
 
 func _physics_process(_delta):
-	position = target.position
+	if target:
+d		position = target.position
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,49 +14,28 @@ var mouse_delta = Vector2.ZERO
 var enemies_in_hurtbox = []
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$AnimationPlayer.play("Idle")
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	if Input.is_action_pressed("forward"):
-		direction.z += 1
-		$Turnip.rotation.y = deg_to_rad(0)
-	if Input.is_action_pressed("back"):
 		direction.z -= 1
-		$Turnip.rotation.y = deg_to_rad(180)
+	if Input.is_action_pressed("back"):
+		direction.z += 1
 	if Input.is_action_pressed("left"):
-		direction.x += 1
-		$Turnip.rotation.y = deg_to_rad(90)
-	if Input.is_action_pressed("right"):
 		direction.x -= 1
-		$Turnip.rotation.y = deg_to_rad(-90)
+	if Input.is_action_pressed("right"):
+		direction.x += 1
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
+		velocity = direction * movement_speed * delta
+		velocity = velocity.rotated(Vector3.UP, rotation.y)
+		$Turnip.rotation.y = atan2(direction.x, direction.z)
+		move_and_slide()
 		if $AnimationPlayer.current_animation != "Attack":
-			$AnimationPlayer.play("Run")
-	elif $AnimationPlayer.current_animation != "Attack":
+			$AnimationPlayer.play("Run")	
+		elif $AnimationPlayer.current_animation != "Attack":
 			$AnimationPlayer.play("Idle")
-	velocity = direction * movement_speed * delta
-	velocity = velocity.rotated(Vector3.UP, rotation.y)
-	move_and_slide()
-
-func _process(delta):
-	if Input.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotation.x += mouse_delta.y * sensitivity * delta
-		rotation.y -= mouse_delta.x * sensitivity * delta
-		rotation.x = clamp(rotation.x, deg_to_rad(min_pitch_deg), deg_to_rad(max_pitch_deg))
-		mouse_delta = Vector2.ZERO
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		mouse_delta = event.relative
-	if event is InputEventMouseButton:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		if event.is_action("attack"):
-			attack()
 
 func attack():
 	for enemy in enemies_in_hurtbox:
