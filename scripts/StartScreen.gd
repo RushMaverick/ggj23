7b51014f0d65570446e30a1292c737f935a4715e
Doxakis1 <mike.karat@yahoo.com>
2023-02-03 13:02:15 +0200

    Fixed mouse bug, get it.... bug

--- scripts/StartScreen.gd
+++ scripts/StartScreen.gd
@@ -3,6 +3,8 @@ extends Control
 func _on_quit_pressed():
 	get_tree().quit()
 
-
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
