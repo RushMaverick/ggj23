af5ef526e4f0b7b24eaa6098d0e013e3acc4f8cb
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-02-04 19:56:37 +0200

    Made new menu functional

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -9,6 +9,15 @@ var quit_z: float  = 0.0
 var is_start_hovered: bool = false
 var is_quit_hovered: bool = false
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _input(event):
+	if event.is_action_pressed("attack") and is_quit_hovered:
+		get_tree().quit()
+	if event.is_action_pressed("attack") and is_start_hovered:
+		get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
 func _physics_process(delta):
 	$StartGameMenuItem.position.z = lerp($StartGameMenuItem.position.z, start_z, hover_animation_speed * delta)
 	$QuitMenuItem.position.z = lerp($QuitMenuItem.position.z, quit_z, hover_animation_speed * delta)
