3ba54439eec5a452485876dc2edc31df49c22105
Tuomas <tuomas.jaasalo@alceon.fi>
2023-02-05 07:11:45 +0200

    Fixed intro sequence delay

--- scripts/Intro.gd
+++ scripts/Intro.gd
@@ -1,4 +1,10 @@
 extends Node3D
 
+func _ready():
+	$Timer.start()
+
 func _on_animation_player_animation_finished(_anim_name):
 	get_tree().change_scene_to_file("res://scenes/NewMenu.tscn")
+
+func _on_timer_timeout():
+	$AnimationPlayer.play("Intro")
